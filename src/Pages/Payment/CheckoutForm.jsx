e30729cc70890a64a0187e58f8190c8528464e25
Farzana Mohsin <fmohsin@usertesting.com>
2024-06-07 16:18:08 -0400

    add checkout file to handle the payment and error

---
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -0,0 +1,64 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+const CheckoutForm = () => {
+  const [error, setError] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card", // type of payment is card
+      card, // card data received
+    });
+
+    if (error) {
+      console.log("[error]", error);
+      setError(error.message);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+      setError("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className='btn rounded-none btn-outline my-5'
+          type='submit'
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+        <p className='text-red-600'>{error}</p>
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,10 +1,20 @@
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 const Payment = () => {
+  const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+
   return (
     <div>
       <SectionTitle heading='Payment'></SectionTitle>
       <h2>Payment page</h2>
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
+      </div>
     </div>
   );
 };

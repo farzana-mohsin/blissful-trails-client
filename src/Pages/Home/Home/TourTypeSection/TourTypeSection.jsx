d8cb74e0cedb5d75b202758c43fc8facf09c2bc5
Farzana Mohsin <fmohsin@usertesting.com>
2024-06-09 09:44:29 -0400

    add styling to tour type section and make it responsive

--- src/Pages/Home/Home/TourTypeSection/TourTypeSection.jsx
+++ src/Pages/Home/Home/TourTypeSection/TourTypeSection.jsx
@@ -21,24 +21,58 @@ const TourTypeSection = () => {
   // const cruises = packages.filter((pack) => pack.tourType === "cruises");
 
   return (
-    <div>
+    <div className="bg-[url('https://i.ibb.co/ZMS3P1w/tropic-leaf-background-jungle-leaves-dark-plants-tropics-wet-greens-wallpaper-169428834.webp')] text-white py-10 object-cover my-20">
       <SectionTitle
         heading='Choose Tour'
         subHeading='Find your next travel adventure and make it memorable. Explore wildlife, enjoy seaside or book a cruise tour. Check out our popular destinations.'
       ></SectionTitle>
 
-      <Link to='/packages/seaside'>
-        <button>Seaside</button>
-      </Link>
-      <Link to='/packages/hiking'>
-        <button>Hiking</button>
-      </Link>
-      <Link to='/packages/wildlife'>
-        <button>Wildlife</button>
-      </Link>
-      <Link to='/packages/cruises'>
-        <button>Cruises</button>
-      </Link>
+      <div className=' text-white p-8 flex items-center justify-evenly mt-5'>
+        <Link to='/packages/seaside'>
+          <div className='flex flex-col items-center space-y-5'>
+            <span>
+              <img
+                src='	https://wanderers.qodeinteractive.com/wp-content/uploads/2018/03/h1-custom-icon-5-hover.png'
+                alt=''
+              />
+            </span>
+            <button>Seaside</button>
+          </div>
+        </Link>
+        <Link to='/packages/hiking'>
+          <div className='flex flex-col items-center space-y-5'>
+            <span>
+              <img
+                src='https://wanderers.qodeinteractive.com/wp-content/uploads/2018/03/h1-custom-icon-3-hover.png'
+                alt=''
+              />
+            </span>
+            <button>Hiking</button>
+          </div>
+        </Link>
+        <Link to='/packages/wildlife'>
+          <div className='flex flex-col items-center space-y-5'>
+            <span>
+              <img
+                src='	https://wanderers.qodeinteractive.com/wp-content/uploads/2018/03/h1-custom-icon-4-hover.png'
+                alt=''
+              />
+            </span>
+            <button>Wildlife</button>
+          </div>
+        </Link>
+        <Link to='/packages/cruises'>
+          <div className='flex flex-col items-center space-y-5'>
+            <span>
+              <img
+                src='	https://wanderers.qodeinteractive.com/wp-content/uploads/2018/03/h1-custom-icon-2-hover.png'
+                alt=''
+              />
+            </span>
+            <button>Cruises</button>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
